31f7cec191bcbe6f86dc2e557ac51b8dd676263c
rahmatmubini <rahmat.mubini12@gmail.com>
2022-09-05 10:33:38 +0800

    memperbaiki layout main halaman utaman dan memperbaiki navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,53 @@
-<header class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow font-mono text-xl">
+<header class="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+    <div class="container">
+        <div class="flex items-center justify-between relative">
+            <div class="px-4">
+                <a href="#home" class="font-bold text-lg text-hijau block py-6">ToFo PUBG</a>
+            </div>
+            <div class="flex items-center px-4">
+                <button id="hamburger" name="hamburger" type="button" class="block absolute right-4 lg:hidden">
+                    <span class="hamburger-line transiton duration-300 ease-in-out origin-top-left"></span>
+                    <span class="hamburger-line transiton duration-300 ease-in-out"></span>
+                    <span class="hamburger-line transiton duration-300 ease-in-out origin-bottom-left"></span>
+                </button>
+
+            <nav id="nav-menu" class="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full  lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
+                <ul class="block lg:flex">
+                    <li class="group">
+                    <a href="/" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">Beranda</a>
+                    </li>
+                    <li class="group">
+                    <a href="/tournaments" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">Tournaments</a>
+                    </li>
+                @auth
+                    <li class="group">
+                        <a href="/dashboard" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">My Dashboard</a>
+                    </li>
+                    <li class="group">
+                        <a href="/profile" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">My Profile</a>
+                    </li>
+                    <li>
+                        <form action="/logout" method="post">
+                            @csrf
+                                <button type="submit" class="">Logout</button>
+                        </form>
+                    </li>
+
+                    <li>
+                        <a class="" href="#">Wellcome back, {{ auth()->user()->name }}</a>
+                    </li>
+                @else
+                    <li class="nav-item ">
+                        <a href="/login" class="nav-link "><i class="bi bi-box-arrow-in-right"></i> Login</a>
+                    </li>
+                @endauth
+                </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+{{-- <header class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow font-mono text-xl">
 
     <a class="ml-2" href="/"></a>
 
@@ -15,9 +64,6 @@
             <li class="nav-item">
                 <a class="nav-link" href="/tournaments">Tournament</a>
             </li>
-            {{-- <li class="nav-item">
-                <a class="nav-link" href="/community">Community</a>
-            </li> --}}
         </ul>
 
         <ul class="navbar-nav ms-auto">
@@ -48,13 +94,4 @@
             @endauth
         </ul>
     </div>
-
-    {{-- <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-            <form action="/logout" method="post">
-                @csrf
-                <button type="submit" class="nav-link px-3 bg-dark border-0">Logout <span data-feather="log-out"></span></button>
-            </form>
-        </div>
-    </div> --}}
-</header>
+</header> --}}
