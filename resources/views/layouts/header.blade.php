a7ca02d0efe7472ddbf735bad082c8a217d51702
rahmatmubini <rahmat.mubini12@gmail.com>
2022-09-11 07:30:19 +0800

    memperbaiki navabar= warna, ukuran, burger-rounded

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,11 @@
-<header class="bg-hijau  top-0 left-0 w-full flex items-center z-10">
+<header class="bg-slate-800  top-0 left-0 w-full flex items-center z-10 h-11">
     <div class="container">
         <div class="flex items-center justify-between relative">
             <div class="px-4">
                 <a href="#home" class="font-bold text-lg text-white block py-6">ToFo PUBG</a>
             </div>
             <div class="flex items-center px-4">
-                <button id="hamburger" name="hamburger" type="button" class="block absolute right-4 lg:hidden">
+                <button id="hamburger" name="hamburger" type="button" class="bg-white block absolute right-4 lg:hidden rounded">
                     <span class="hamburger-line transiton duration-300 ease-in-out origin-top-left"></span>
                     <span class="hamburger-line transiton duration-300 ease-in-out"></span>
                     <span class="hamburger-line transiton duration-300 ease-in-out origin-bottom-left"></span>
@@ -14,30 +14,27 @@
             <nav id="nav-menu" class="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full  lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
                 <ul class="block lg:flex">
                     <li class="group">
-                    <a href="/" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">Beranda</a>
-                    </li>
-                    <li class="group">
-                    <a href="/tournaments" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">Tournaments</a>
+                    <a href="/tournaments" class="text-lg lg:text-white text-hijau py-2 mx-8 flex group-hover:text-hijau">Tournaments</a>
                     </li>
                 @auth
                     <li class="group">
-                        <a href="/dashboard" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">My Dashboard</a>
+                        <a href="/dashboard" class="text-lg lg:text-white text-hijau py-2 mx-8 flex group-hover:text-hijau">My Dashboard</a>
                     </li>
                     <li class="group">
-                        <a href="/profile" class="text-base text-hitam py-2 mx-8 flex group-hover:text-hijau">My Profile</a>
+                        <a href="/profile" class="text-lg lg:text-white text-hijau py-2 mx-8 flex group-hover:text-hijau">My Profile</a>
                     </li>
                     <li class="group mt-2">
-                        <a class="" href="#">Wellcome back, {{ auth()->user()->name }}</a>
+                        <a class="ml-7 text-lg lg:text-white text-black" href="#">Wellcome back, {{ auth()->user()->name }}</a>
                     </li>
                     <li class="group ml-7 mt-2">
                         <form action="/logout" method="post">
                             @csrf
-                                <button type="submit" class="">Logout</button>
+                                <button type="submit" class="lg:text-white text-hijau text-lg">Logout</button>
                         </form>
                     </li>
                 @else
-                    <li class="nav-item ">
-                        <a href="/login" class="nav-link "><i class="bi bi-box-arrow-in-right"></i> Login</a>
+                    <li class="group lg:mt-2 lg:ml-auto ml-8">
+                        <a href="/login" class="nav-link text-lg lg:text-white text-hijau"><i class="bi bi-box-arrow-in-right text-lg lg:text-white text-hijau"></i> Login</a>
                     </li>
                 @endauth
                 </ul>
@@ -46,51 +43,3 @@
         </div>
     </div>
 </header>
-{{-- <header class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow font-mono text-xl">
-
-    <a class="ml-2" href="/"></a>
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <a class="text-white col-md-3 col-lg-2 me-0 px-3" href="/">TuruDeck</a>
-        <ul class="navbar-nav pl-8">
-            <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/tournaments">Tournament</a>
-            </li>
-        </ul>
-
-        <ul class="navbar-nav ms-auto">
-            @auth
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Wellcome back, {{ auth()->user()->name }}
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="/profile"><i class="bi bi-person"></i> My Profile</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li>
-                            <form action="/logout" method="post">
-                                @csrf
-                                <button type="submit" class="dropdown-item">
-                                    <i class="bi bi-box-arrow-right"></i> Logout
-                                </button>
-                            </form>
-                        </li>
-                    </ul>
-                </li>
-            @else
-                <li class="nav-item ">
-                    <a href="/login" class="nav-link "><i class="bi bi-box-arrow-in-right"></i> Login</a>
-                </li>
-            @endauth
-        </ul>
-    </div>
-</header> --}}
